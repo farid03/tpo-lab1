c766ba6abc107de250a561ca98dc2c3a4c46e8cc
farid03 <71608691+farid03@users.noreply.github.com>
2023-02-20 14:06:11 +0300

    removed useless properties

--- src/main/java/task3/Chair.java
+++ src/main/java/task3/Chair.java
@@ -6,13 +6,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Chair {
-    private final Color color;
-    private final int comfortability;
     boolean sit;
 
-    public Chair(final Color color, final int comfortability, final boolean sit) {
-        this.color = color;
-        this.comfortability = comfortability;
+    public Chair(final boolean sit) {
         this.sit = sit;
     }
 
